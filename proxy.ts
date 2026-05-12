d2873ddd1ceb296e056e90431bb8403f2ce7b397
Lâm Thành Duy <thanhduythptthanhbinh@gmail.com>
2026-05-12 19:48:29 +0700

    [Update] proxy

--- proxy.ts
+++ proxy.ts
@@ -1,7 +1,7 @@
 import { NextRequest, NextResponse } from "next/server";
 import { jwtVerify } from "jose"; // Sử dụng jose thay vì jsonwebtoken
 
-export async function middleware(request: NextRequest) {
+export async function proxy(request: NextRequest) {
   const token = request.cookies.get("adminToken")?.value;
   const isLoginPage = request.nextUrl.pathname === "/admin/login";
   const secret = new TextEncoder().encode("DONGUYENDIEUANH");
